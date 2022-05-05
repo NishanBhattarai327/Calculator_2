aa5224dcff15902393177d84abb0f9a45cf7cdcf
NishanBhattarai327 <bhattarainishan327@gmail.com>
2022-05-05 08:54:44 +0545

    Add Calculator module

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const Calculator = (function() {
+ 	const add = (x, y) => x + y;
+ 	const divide = (x, y) => x / y;
+ 	const multiply = (x, y) => x * y;
+ 	const substract = (x, y) => x - y;
+
+ 	return {
+ 		add, divide, multiply, substract
+ 	};
+})();
+
+console.log(Calculator.add(44, 99));
